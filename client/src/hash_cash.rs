f42f00f65dddd5929a4e3f9346a4b72149ec5956
kelyan-9560 <kelyan.bervin@gmail.com>
2022-06-29 16:38:53 +0200

    hash_code

---
+++ client/src/hash_cash.rs
@@ -0,0 +1,57 @@
+use common::{MD5HashCashInput, MD5HashCashOutput};
+
+
+
+pub fn count_zero_at_the_beginning(word: String) -> u8{
+    let mut count = 0;
+
+    for i in word.chars() {
+        if i == '0' {
+            count = count + 1;
+        } else {
+            return count;
+        }
+    }
+    return panic!("mince");
+}
+
+
+
+pub fn hash_cash(input: MD5HashCashInput) -> MD5HashCashOutput{
+
+    let seed = "";
+    //let mut seed: &str;
+
+    let adds = "adds";
+    //let adds = input.message;
+
+    
+    let hash_code:String = "".to_string();
+    let mut its_good = false;
+
+    while !its_good {
+        let concat = seed.to_string() + adds;
+        println!("concat = {}", concat);
+
+        let hash_code = md5::compute(concat);
+        println!("hash_code = {:x}", hash_code);
+
+        let nb_zero = count_zero_at_the_beginning(format!("{:X}", hash_code));
+        println!("nb_zero = {:x}", nb_zero);
+
+        if (nb_zero * 8) >= input.complexity as u8 {
+            its_good = true;
+        } else {
+            //caractere_en_plus + 1hexa
+            // seed = seed + 1hexa
+            
+        }
+    }
+
+    let response = MD5HashCashOutput {
+        seed: seed.parse().unwrap(),
+        hashcode: hash_code
+    };
+
+    return response;
+}
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,29 +1,60 @@
-mod HashCash;
+extern crate core;
 
+mod hash_cash;
+
+use common::{Message, Welcome};
 use std::io::Write;
 use std::net::TcpStream;
-
-
-
-
-
-
-
+use serde::{Serialize, Deserialize};
+use crate::hash_cash::count_zero_at_the_beginning;
 
 
 fn main(){
-    let stream = TcpStream::connect("localhost:7676");
+
+    //se connecter
+    let stream = TcpStream::connect("localhost:7878");
 
     match stream {
         Ok(mut stream) => {
 
-            // Serialize la structure
+            //Envoyer un msg Hello, Welcome, Subscribe au serveur
+                //La transformer en octets avec .as_bytes();
+
+            //let message = Message::Hello.as_bytes();
 
 
-            //La transformer en octets avec .as_bytes();
-            let messages = "Hello".as_bytes();
-            let response = stream.write_all(messages);
+
+            //convertir le msg en octets
+            let message = "Hello".as_bytes();
+            // le serialiser
+            let serialized = serde_json::to_string(&message).unwrap();
+
+            //prendre la taille (en u32) du message serialisé
+            let size:u32 = serialized.len() as u32;
+
+            // le convertir en bigendian avec la fonction to_be_bytes()
+                //Les chiffres sont en réprésenté différement en fonction
+                //du système d'exploitation, bigendian est la convention pour tout les OS
+            let bigendian_size = size.to_be_bytes();
+
+            //envoyer la taille
+                // puis le message
+            //let response = stream.write_all(serialized);
+
+
+            //faire le process inverse pour recevoir les messages
+
         }
         Err(err) => panic!("Cannot connect : {err}")
     }
+
+
+/*
+    println!("------------");
+    let test = String::from("0000g0reen");
+    println!("{}", count_zero_at_the_beginning(test));
+
+ */
+
+
 }
