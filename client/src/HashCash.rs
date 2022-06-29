728b309f023c2d8041a4a446539362d1bbab9573
kelyan-9560 <kelyan.bervin@gmail.com>
2022-06-29 14:22:49 +0200

    refacto concat and hash_code

--- client/src/HashCash.rs
+++ client/src/HashCash.rs
@@ -2,30 +2,33 @@ use std::hash::Hash;
 use common::{MD5HashCashInput, MD5HashCashOutput};
 use md5::{Digest};
 
-fn hash_cash(input: MD5HashCashInput){
-
-    let its_good = false;
-    let mut adds = "";
-    let mut res = "";
+fn hash_cash(input: MD5HashCashInput) {
+    let mut its_good = false;
+    let adds = "adds";
+    let mut res = "res";
 
 
     while !its_good {
+        let concat = res.to_string() + adds;
+        println!("concat = {}", concat);
 
-        res = &*[adds, &input.message].join("");
+        let hash_code = md5::compute(concat);
+        println!("hash_code = {:x}", hash_code);
 
 
+        //compter les zero au debut du hash_code
 
-        let hash_code = md5::compute(res);
+        /*
+            if (zero * 8 >= complexite ){
+                cestbon = true
+                //(res, hash);
+            }
+        */
 
 
-        println!("{:x}", digest(hash_code));
+        //caractere_en_plus + 1hexa
 
-        //print!("{:?}", hash_code)
 
+        its_good = true;
     }
-
-
-
-
-
 }
