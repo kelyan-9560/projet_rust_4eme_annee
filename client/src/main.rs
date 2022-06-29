345d884323d23df3c58bd4ca18e2724704fff296
kelyan-9560 <kelyan.bervin@gmail.com>
2022-06-29 18:52:54 +0200

    split client main

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,48 +3,80 @@ extern crate core;
 mod hash_cash;
 
 use common::{Message, Welcome};
-use std::io::Write;
-use std::net::TcpStream;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 use serde::{Serialize, Deserialize};
 use crate::hash_cash::count_zero_at_the_beginning;
 
 
+
+
+fn send_message_to_server(mut stream: TcpStream, message: Message){
+    // serialiser le message
+    //struct -> json -> byte
+    //let message = Message::Hello;
+    let serialized_message = serde_json::to_string(&message).unwrap();
+    print!("{:?}", serialized_message);
+
+    let message_as_bytes = serialized_message.as_bytes();
+    print!("{:?}", message_as_bytes);
+
+    //prendre la taille (en u32) du message serialisé
+    let size:u32 = serialized_message.len() as u32;
+    print!("{:?}", size);
+
+    // le convertir en bigendian avec la fonction to_be_bytes()
+    //Les chiffres sont en réprésenté différement en fonction
+    //du système d'exploitation, bigendian est la convention pour tout les OS
+    let bigendian_size = size.to_be_bytes();
+    print!("{:?}", bigendian_size);
+
+    //envoyer la taille
+    stream.write(&bigendian_size).expect("Error: send size failed !");
+    // puis le message
+    stream.write(serialized_message.as_ref()).expect("Error: Send message failed !");
+}
+
+
+/*
+fn server_message_reception(mut stream: TcpStream){
+    //faire le process inverse pour recevoir les messages
+
+    let size = stream.read();
+    let message = stream.read();
+
+}
+*/
+
+
 fn main(){
 
-    //se connecter
     let stream = TcpStream::connect("localhost:7878");
 
     match stream {
         Ok(mut stream) => {
 
-            // serialiser le message
-                //struct -> json -> byte
             let message = Message::Hello;
-            let serialized_message = serde_json::to_string(&message).unwrap();
-            print!("{:?}", serialized_message);
-
-            let message_as_bytes = serialized_message.as_bytes();
-            print!("{:?}", message_as_bytes);
+            send_message_to_server(stream, message);
 
 
-            //prendre la taille (en u32) du message serialisé
-            let size:u32 = serialized_message.len() as u32;
-            print!("{:?}", size);
+            //TODO : Récupérer les messages envoyés par le serveur
+            /*
+            let listener = TcpListener::bind(&stream);
 
-            // le convertir en bigendian avec la fonction to_be_bytes()
-                //Les chiffres sont en réprésenté différement en fonction
-                //du système d'exploitation, bigendian est la convention pour tout les OS
-            let bigendian_size = size.to_be_bytes();
-            print!("{:?}", bigendian_size);
+            let listener = match listener {
+                Ok(l) => l,
+                Err(err) => panic!("Cannot connect : {err}")
+            };
 
-            //envoyer la taille
-            stream.write(&bigendian_size).expect("Error: send size failed !");
-                // puis le message
-            stream.write(serialized_message.as_ref()).expect("Error: Send message failed !");
+            for message in listener.incoming(){
+                let mut input_message = message.unwrap();
+                println!("input_message {:?}", input_message);
 
+                input_message.read_to_end();
 
-            //faire le process inverse pour recevoir les messages
-
+            }
+             */
         }
         Err(err) => panic!("Cannot connect : {err}")
     }
