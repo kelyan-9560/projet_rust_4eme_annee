ded69fb0357b6d17e239505e1f48d1a1a4da5514
kelyan-9560 <kelyan.bervin@gmail.com>
2022-07-07 00:55:31 +0200

    remove comment and Warning in main.rs

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,13 +1,11 @@
-mod HashCash;
-mod recover_secret;
 extern crate core;
 
 mod hash_cash;
+mod recover_secret;
 
 use common::{Message, Subscribe};
 use std::io::{Read, Write};
 use std::net::{TcpStream};
-use byteorder::{ReadBytesExt, WriteBytesExt, BigEndian, LittleEndian};
 
 
 
@@ -55,10 +53,10 @@ fn server_message_reception(mut stream: &TcpStream){
 
     //Receptionner la taille
     let mut size : Vec<u8> = vec![0; 10];
-    let mut read_result = &stream.read(&mut size); // size_result == est ce que ça s'est bien passé
-    // le resultat du read est dans size
+    let _read_result = &stream.read(&mut size); // size_result == est ce que ça s'est bien passé
+                                                                    // le resultat du read est dans size
     println!("size : {:?}", size);
-    let mut size_u32 = get_size_from_message(size);
+    let size_u32 = get_size_from_message(size);
     //     convertir de bigendian à u32 (voir byteorder dependencies)
     //  let mut size_u32 = size.read_u32::<BigEndian>();
     //let mut size_u32 = size as u32;
@@ -69,7 +67,7 @@ fn server_message_reception(mut stream: &TcpStream){
 
     //Receptionner le message
     let mut input_message : Vec<u8> = vec![0; size_u32 as usize];
-    let mut read_message = &stream.read(&mut input_message);
+    let mut _read_message = &stream.read(&mut input_message);
     println!("input_message {:?}", input_message);
 
     //mettre le message dans le vecteur de la taille de size
@@ -90,52 +88,13 @@ fn main(){
     let stream = TcpStream::connect("localhost:7878");
 
     match stream {
-        Ok(mut stream) => {
+        Ok(stream) => {
 
             let message = Message::Hello;
             send_message_to_server(&stream, message);
 
             println!("-------------------");
 
-            //TODO : Récupérer les messages envoyés par le serveur
-
-/*
-            //convertir le taille bigendian -> u32 -> usize
-
-            //Receptionner la taille
-            let mut size : Vec<u8> = vec![0; 10];
-            let mut read_result = &stream.read(&mut size); // size_result == est ce que ça s'est bien passé
-                                                                  // le resultat du read est dans size
-            println!("size : {:?}", size);
-            let mut size_u32 = 0;
-            for i in size {
-                if i != 0 { 
-                    size_u32 = i;
-                }
-            }
-            //     convertir de bigendian à u32 (voir byteorder dependencies)
-            //  let mut size_u32 = size.read_u32::<BigEndian>();
-            //let mut size_u32 = size as u32;
-            //  let mut size_u32 = size.to_le_bytes();
-            //  let mut size_u32 = read_result.unwrap();
-            //  let mut size_u32 = String::from_utf8_lossy(&*size);
-            println!("size as u32 : {:?}", size_u32);
-
-            //Receptionner le message
-            let mut input_message : Vec<u8> = vec![0; size_u32 as usize];
-            let mut read_message = &stream.read(&mut input_message);
-            println!("input_message {:?}", input_message);
-
-            //mettre le message dans le vecteur de la taille de size
-            //let mut message_vector = vec![&input_message; &size[0] as usize];
-
-            //      passer de l'octet au String
-            let input_message_str = String::from_utf8_lossy(&input_message);
-            println!("input_message_str {:?}", input_message_str);
-            //       le deserialiser
-            let deserialized_message: Message = serde_json::from_str(&input_message_str).unwrap();
-            println!("deserialized = {:?}", deserialized_message);
-*/
 
             server_message_reception(&stream);
 
