e5bb83abe80561487e181f015569882c021c0deb
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-07-07 12:48:08 +0200

    :white_check_mark: tests recover_sercret (from example in subject). Doesn't work, i dont know if recover secret is finish

--- client/src/recover_secret.rs
+++ client/src/recover_secret.rs
@@ -32,3 +32,36 @@ fn sentence_formatter(sentence: Vec<char>) -> String{
 
     return a_string;
 }
+
+
+#[cfg(test)]
+mod tests {
+    use common::RecoverSecretInput;
+    use crate::recover_secret::recover_secret;
+
+    #[test]
+    fn should_test_recover_secret(){
+        let value = RecoverSecretInput{
+            word_count: 2,
+            letters: "t cCehuCethoCeschouC'schout h".to_string(),
+            tuple_sizes: vec![3, 4, 5, 7, 7, 3]
+        };
+        let res = recover_secret(value);
+        println!("{}",res);
+
+        assert_eq!(res,"C'est chou");
+    }
+
+    #[test]
+    fn should_test_recover_secret2(){
+        let value = RecoverSecretInput{
+            word_count : 1,
+            letters : "WvyOAlxafUzleiSOl9xayBeHTmy9xWTU5lMW4nUO5lMWRajn2BiHSRUzy5afnUz5wlexWrm5wlBWr4mAlBrUmzHxTUzwlHrfTwBeSRmzlMSRfoUOAe9S4oUiraOiramzM5w3l".to_string(),
+            tuple_sizes : vec![6, 8, 4, 6, 4, 7, 8, 9, 6, 9, 8, 7, 5, 7, 6, 6, 9, 5, 4, 5, 4]
+        };
+        let res = recover_secret(value);
+        println!("{}",res);
+
+        assert_eq!(res,"xWRvraj4fonTUmzyO25wA3lBeiM9H");
+    }
+}
